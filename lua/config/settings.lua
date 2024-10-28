2de7a149b9773a37572041c8170e8abdf654495b
LoBombadil <louis.granvisir-clerc@iut-dijon.u-bourgogne.fr>
2024-10-28 13:49:03 +0100

    nvim conf

---
+++ lua/config/settings.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.autoindent = true
+vim.opt.cursorline = true
+
+vim.opt.smartindent = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+vim.opt.colorcolumn = "80"
---
+++ lua/plugins/autocomplete.lua
@@ -0,0 +1,82 @@
+return {
+  -- Plugin de complétion principale
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",       -- Support de LSP
+      "hrsh7th/cmp-buffer",         -- Complétion pour le buffer actuel
+      "hrsh7th/cmp-path",           -- Complétion pour les chemins de fichiers
+      "saadparwaiz1/cmp_luasnip",   -- Intégration avec LuaSnip
+      {
+        "tzachar/cmp-tabnine",
+        build = "./install.sh",     -- Installe Tabnine
+        config = function()
+          require("cmp_tabnine.config"):setup {
+            max_lines = 1000,
+            max_num_results = 3,
+            sort = true,
+          }
+        end,
+      },
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = {
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirme la complétion avec "Entrée"
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+          { name = "cmp_tabnine" },
+          { name = "luasnip" },
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            if entry.source.name == "cmp_tabnine" then
+              vim_item.kind = " [TabNine]"
+            end
+            return vim_item
+          end,
+        },
+      })
+    end,
+  },
+  -- Plugin pour les snippets
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "rafamadriz/friendly-snippets", -- Snippets préconfigurés
+    },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
+}
+
